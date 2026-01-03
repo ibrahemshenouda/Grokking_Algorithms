0ba211e3ba99401b9bf61f1f007d21e168e973b5
Ibrahim Shnouda <eprahimshenouda88@gmail.com>
2026-01-03 14:19:03 +0200

    adding quicksort

---
+++ Chapter_04/02_Quicksort/quicksort.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+def quicksort(array):
+    if len(array) < 2:
+        return array
+
+    pivot = array[0]
+
+    less = [i for i in array[1:] if i <= pivot]
+    greater = [i for i in array[1:] if i > pivot]
+
+    return quicksort(less) + [pivot] + quicksort(greater)
+
+
+print(quicksort([11, 20, 1, 3, -5]))
