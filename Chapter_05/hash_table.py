6009c91b1b0cd16aac129f1027ebb5a3d62f69dd
Ibrahim Shnouda <eprahimshenouda88@gmail.com>
2026-01-04 16:33:40 +0200

    adding chapter_05

---
+++ Chapter_05/hash_table.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+# Hash Tables
+# dictionary is a Hash tables
+# Faster than arrays
+# used to prevent duplicate
+# used as a cache in websites
+
+voted = {}
+
+
+def check_voter(name):
+    if name in voted:
+        print("Kick them out!")
+    else:
+        voted[name] = True
+        print("let them vote!")
+
+
+check_voter("tom")
+check_voter("mike")
+check_voter("tom")
+print(voted)
